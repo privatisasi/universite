95188b2105e047b574153b703cac18488e3ae7cb
Arif Setyawan <me@arifsetyawan.com>
2012-12-25 17:10:00 +0700

    Set up grid view

--- application/views/mahasiswa/multi.php
+++ application/views/mahasiswa/multi.php
@@ -1,15 +1,255 @@
-<div class="row">
+<div class="row content">
 	<div class="span12">
 		<?php echo form_open('mhs/search',array('class'=>'form-search', 'method'=>'GET')); ?>
 		<legend>Listing Data Mahasiswa</legend>
 		<div class="control-group">
 			<input type="search" name="search" placeholder="search" class="input-medium search-query"/>
-			<button type="submit" class="btn"> Search </button>
+			<button type="submit" class="btn"> <small>Cari</small></button>
 		</div>
 		<?php echo form_close();?>
 		<p>
 			<small>*) Gunakan form pencarian untuk menemukan data mahasiswa sesuai dengan NIMnya</small>
 		</p>
 		</form>
+	</div>
+</div>
+<div class="row content">
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+				<span class="badge badge-inverse">smart</span>
+				<span class="badge badge-inverse">loud</span>
+				<span class="badge badge-inverse">geek</span>
+				<span class="badge badge-inverse">asisten lab</span>
+				<span class="badge badge-inverse">pkm</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">loud</span>
+				<span class="badge badge-inverse">geek</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">asisten lab</span>
+				<span class="badge badge-inverse">pkm</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+				<span class="badge badge-inverse">smart</span>
+				<span class="badge badge-inverse">loud</span>
+				<span class="badge badge-inverse">geek</span>
+				<span class="badge badge-inverse">asisten lab</span>
+				<span class="badge badge-inverse">pkm</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+				<span class="badge badge-inverse">smart</span>
+				<span class="badge badge-inverse">loud</span>
+				<span class="badge badge-inverse">geek</span>
+				<span class="badge badge-inverse">asisten lab</span>
+				<span class="badge badge-inverse">pkm</span>
+			</div>
+		</div>
+	</div>
+	<div class="span3 mhs">
+		<div class="nim center">
+			<div><p>06560267</p></div>
+		</div>
+		<div class="mhs-list">
+			<p class="lead name">Arif Setyawan</p>
+			<i class="icon-picture"></i> <i class="icon-tasks"></i> <i class="icon-list"></i>
+			<hr/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</p>
+			<hr/>
+			<div class="tweet">
+				<div class="tweet-user"> <i class="icon-comment"></i> @ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			<div class="fb">
+				<div class="fb-user">fb /ariflogic</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, ... </p>
+			</div>
+			<hr/>
+			<div class="reputation">
+				<span class="badge badge-inverse">reader</span>
+				<span class="badge badge-inverse">smart</span>
+				<span class="badge badge-inverse">loud</span>
+				<span class="badge badge-inverse">geek</span>
+				<span class="badge badge-inverse">asisten lab</span>
+				<span class="badge badge-inverse">pkm</span>
+			</div>
+		</div>
 	</div>
 </div>
